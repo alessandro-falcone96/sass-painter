40796484022a209b30df274bc76a513b07bf0ae0
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-04-16 16:36:10 +0200

    Aggiunti i box con foto e testi nel main

--- js/script.js
+++ js/script.js
@@ -16,6 +16,28 @@ var app = new Vue (
         "Formazione e Supporto",
         "Versione di Prova Gratuita",
       ],
+      boxesDiv: [
+        {
+          foto: "https://cc-prod.scene7.com/is/image/CCProdAuthor/dt_ps_riverflow__2?$pjpeg$&jpegSize=200&wid=701",
+          titolo: "Ritocca, sostituisci, assembla, wow!",
+          testo: "Realizza ritratti da manuale con soggetti più espressivi, sostituisci il cielo con un clic, rimuovi gli oggetti, applica ritocchi e ricolora una scena. Grazie a strumenti di fotoritocco avanzati e a nuove funzionalità intelligenti per la creazione di effetti magici, puoi trasformare le immagini in opere d’arte."
+        },
+        {
+          foto: "https://cc-prod.scene7.com/is/image/CCProdAuthor/dt_ps_riverflow__4?$pjpeg$&jpegSize=200&wid=660",
+          titolo: "Fai correre la tua immaginazione per trasformare le tue foto.",
+          testo: "Sfruttando un numero virtualmente illimitato di livelli, selezioni con un clic e facili operazioni di mascheratura, puoi creare rapidamente quello che la tua fotocamera non è in grado di immortalare."
+        },
+        {
+          foto: "https://cc-prod.scene7.com/is/image/CCProdAuthor/dt_ps_riverflow__2?$pjpeg$&jpegSize=200&wid=701",
+          titolo: "Mille tocchi di bravura.",
+          testo: "Dipingi e disegna con migliaia di pennelli personalizzati o creane di nuovi: con Photoshop hai a disposizione la più grande raccolta al mondo."
+        },
+        {
+          foto: "https://cc-prod.scene7.com/is/image/CCProdAuthor/dt_ps_riverflow__4?$pjpeg$&jpegSize=200&wid=660",
+          titolo: "Il tuo punto di riferimento per progettare grafiche di grande impatto.",
+          testo: "Poster, packaging, banner, siti web: la progettazione grafica inizia con Photoshop. Combina foto, grafiche e testi per creare immagini completamente nuove, seleziona i soggetti con un clic e gioca con colori ed effetti per perfezionare ogni progetto di design."
+        }
+      ],
     },
     methods: {
 
