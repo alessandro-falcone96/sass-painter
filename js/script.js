e578192f494379b976c352f0639d9c289aee9ddf
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-04-16 16:00:19 +0200

    Aggiunto header

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var app = new Vue (
+  {
+    el: "#app",
+    data: {
+      listaHeaderSopra: [
+        "Creatività e Design",
+        "Marketing ed e-commerce",
+        "PDF ed e-Signature",
+        "Soluzioni Aziendali",
+        "Supporto",
+      ],
+      listaHeaderSotto: [
+        "Adobe Photoshop",
+        "Funzionalità per iPad",
+        "Funzionalità",
+        "Formazione e Supporto",
+        "Versione di Prova Gratuita",
+      ],
+    },
+    methods: {
+
+    }
+  }
+)
